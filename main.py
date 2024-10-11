243627008b88b237d6b6c211467c60635a7c5349
THE D3V1L <151039731+Srinunaik000@users.noreply.github.com>
2024-10-11 19:48:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import argparse
 from modules.port_scanner import main as port_scanner_main
 
+
+def print_banner():
+    ascii_art = """
+     ____            _     ____                                  
+    |  _ \\ ___  _ __| |_  / ___|  ___ __ _ _ __  _ __   ___ _ __ 
+    | |_) / _ \\| '__| __| \\___ \\ / __/ _` | '_ \\| '_ \\ / _ \\ '__|
+    |  __/ (_) | |  | |_   ___) | (_| (_| | | | | | | |  __/ |   
+    |_|   \\___/|_|   \\__| |____/ \\___\\__,_|_| |_|_| |_|\\___|_|   
+    """
+    print(ascii_art)
+    print("Author: SrinuNaik (D3V1L)\n")
+
 def main():
+    print_banner()  # Call to print the banner at the start of the script
+
     parser = argparse.ArgumentParser(description='Bug Bounty Tool')
     subparsers = parser.add_subparsers(dest='command')
 
